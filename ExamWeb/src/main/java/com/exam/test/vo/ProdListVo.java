0043bb42398510821681dc7db249016bec442300
youngseo <vnslduath@naver.com>
2025-03-14 17:39:04 +0900

    03/14 영서

---
+++ ExamWeb/src/main/java/com/exam/test/vo/ProdListVo.java
@@ -0,0 +1,19 @@
+package com.exam.test.vo;
+
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class ProdListVo {
+	private int prodNo;
+	private String prodName;
+	private int price;
+	private String content;
+	private Date createDate;
+	private String image;
+	private String pdf;
+	private int count;
+	private String userId;
+	private String categoryNo;
+}
